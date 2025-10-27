26603c7e271ff1f46ac9eecfd5451b269f387e6a
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-10-27 18:56:08 -0500

    Re-added missing PipeEnd functions

--- libraries/transmission-cpp/src/Pipe.h
+++ libraries/transmission-cpp/src/Pipe.h
@@ -11,6 +11,8 @@ class EXPORT PipeEnd : public Connection {
     PipeEnd(InterruptSafeRingBuffer<char, 4096>& read_buf,
             InterruptSafeRingBuffer<char, 4096>& write_buf);
 
+    [[nodiscard]] int tryReadOne() override;
+    [[nodiscard]] char readOne() override;
     [[nodiscard]] std::vector<char> read(int size) override;
     void write(std::vector<char> bs) override;
 
