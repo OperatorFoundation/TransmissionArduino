eca7857f8263920a8ba962b5af81c7d46feab0e1
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2025-11-20 19:41:05 -0600

    read(int size) refactored in terms of readOne()

--- libraries/transmission-esp32/src/ReliableConnectionUsbCdc.cpp
+++ libraries/transmission-esp32/src/ReliableConnectionUsbCdc.cpp
@@ -141,58 +141,19 @@ std::vector<char> ReliableConnectionUsbCdc::read() {
     return results;
 }
 
-std::vector<char> ReliableConnectionUsbCdc::read(int size) {
+std::vector<char> ReliableConnectionUsbCdc::read(int size)
+{
     std::vector<char> results;
     results.reserve(size);
 
     if(logger) { logger->debugf("read(%d)\n", size); }
 
     // Block until we have 'size' bytes
-    while (results.size() < size) {
-        // Pull any available data from Serial into ring buffer
-        while (Serial.available() > 0 && ring.available() < ring.capacity()) {
-            int byte = Serial.read();
-            if(logger) { logger->debugf("+0x%08X (%d)", byte, ring.count()+1); }
-            if (byte >= 0) {
-                if (!ring.put(static_cast<char>(byte))) {
-                    buffer_full = true;
-                    if(logger) { logger->debugf("!"); }
-                    break;
-                }
-
-                // Check flow control
-                if (!paused && xonXoffEnabled && ring.shouldSendXOFF()) {
-                    Serial.write(XOFF);
-                    paused = true;
-                }
-            }
-            else
-            {
-                if(logger) { logger->debugf("X"); }
-            }
-        }
-
-        // Drain the ring buffer
-        char c;
-        while (ring.get(c) && results.size() < size) {
-            results.push_back(c);
-            if(logger) { logger->debugf("-0x%08X (%d)", c, ring.count()); }
-        }
-
-        // If we still don't have enough, wait a bit
-        bool firstWait = true;
-        if (results.size() < size) {
-            if(logger && firstWait) { firstWait = false; logger->debugf("."); }
-            while(1) { if(Serial.available() > 0) { break; } } // FIXME - remove, this is for debugging
-            yield();  // Let other tasks run
-        }
-    }
-
-    // Check if we should resume flow
-    if (paused && xonXoffEnabled && ring.shouldSendXON()) {
-        if(logger) { logger->debugf("XON"); }
-        Serial.write(XON);
-        paused = false;
+    while (results.size() < size)
+    {
+        int c = readOne();
+        results.push_back(c);
+        if(logger) { logger->debugf("-0x%08X (%d)", c, ring.count()); }
     }
 
     return results;
